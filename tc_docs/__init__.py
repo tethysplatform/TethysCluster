389ddb6cdd2931aef824c1937c7ea847c8cfe17a
Scott <sdc50@byu.net>
2016-01-28 15:46:12 -0700

    tc docs

---
+++ tc_docs/__init__.py
@@ -0,0 +1,10 @@
+"""
+********************************************************************************
+* Project: TethysCluster
+* Name: __init__.py
+* Author: Scott Christensen
+* Created On: 28 January 2016
+* Copyright: (c) Brigham Young University 2016
+* License: BSD 2-Clause
+********************************************************************************
+"""
